35133cad5ef81463ffaa76159e93cb485ce15bc3
Akayiz <ahmed.khaled.yousry@gmail.com>
2024-03-03 19:43:49 +0200

    feat: Generate .md release notes from normal commits based on type
    The release notes markdown contains sections, each section represents a conventional commit type (Bug fixes (fix), New Features (feat), Code Refactoring (refactor)) each section contains the commit message of the normal commit without the commit type and with the first letter of the commit message capitalized

---
+++ release_notes_generator.cpp
@@ -0,0 +1,121 @@
+// Author: Ahmed Khaled
+// Date: 01-March-2024
+// Description: This is a script made as an exercise to demonstrate the ability to work on the Synfig GSoC 24
+// "Automated release notes generator" project, the script generates release notes from conventional git commits
+// Synfig : https://www.synfig.org/
+// Project : https://synfig-docs-dev.readthedocs.io/en/latest/gsoc/2024/ideas.html#projects-ideas
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+const int MAX_DISPLAYED_COMMITS_PER_TYPE = 3;
+const int COMMIT_TYPES_COUNT = 3;
+const int COMMIT_TYPE_INFO_COUNT = 2;
+const string MARKDOWN_OUTPUT_FILE_NAME = "release_notes.md";
+
+enum class Commits{
+    Normal,
+    Merge
+};
+
+// Can later on add HTMl
+enum class OutputTypes{
+    Markdown
+};
+
+enum class ErrorTypes{
+    IncorrectCommitType,
+    NoCommitType,
+};
+
+enum class CommitTypeInfo{
+    ConventionalName,
+    MarkdownTitle
+};
+
+string commitTypes[COMMIT_TYPES_COUNT][COMMIT_TYPE_INFO_COUNT] = {
+    {"fix", "## 🐛 Bug Fixes"}, 
+    {"feat", "## ✨ New Features"},
+    {"refactor", "## ♻️ Code Refactoring"}};
+
+// Add function header
+void generateReleaseNotes(Commits commit, OutputTypes outputType){
+    ofstream outputFile;
+    if(outputType == OutputTypes::Markdown){
+        outputFile.open(MARKDOWN_OUTPUT_FILE_NAME);
+        
+        if (!outputFile.is_open()){
+            cout << "Error: Unable to open markdown release notes file" << endl;
+        }
+    }
+        
+    string commandToRetrieveCommitsMessages;
+    string currentCommitMessage;
+    for (int i = 0; i < COMMIT_TYPES_COUNT; i++)
+    {
+        // Output the title of this commit type section in the release notes
+        outputFile << "\n" << commitTypes[i][(int) CommitTypeInfo::MarkdownTitle] << "\n";
+
+        if(commit == Commits::Normal){
+            // Need to adjust this command to remove commit ids and other useless stuff
+            commandToRetrieveCommitsMessages = "git log --max-count " + to_string(MAX_DISPLAYED_COMMITS_PER_TYPE) + 
+            " --no-merges --oneline --format=\"%s\" --grep=\"^" + commitTypes[i][(int) CommitTypeInfo::ConventionalName] + ": \"";
+
+            //cout << commandToRetrieveCommitsMessages.c_str() << endl;
+            FILE* pipe = popen(commandToRetrieveCommitsMessages.c_str(), "r");
+            if(!pipe){
+                cout << "Error: Unable to open pipe to read git log commmand output" << endl;
+            }
+
+            char buffer[1000];
+            while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+                currentCommitMessage = buffer;
+                // Validating again that the commit message actually starts with the wanted type
+                if(currentCommitMessage.substr(0, currentCommitMessage.find(":")) == commitTypes[i][(int) CommitTypeInfo::ConventionalName])
+                    currentCommitMessage = "- " + currentCommitMessage.substr(currentCommitMessage.find(":") + 2) + "\n";
+                
+                currentCommitMessage[2] = toupper(currentCommitMessage[2]);
+
+                outputFile << currentCommitMessage;
+            }
+        }
+    }
+}
+
+void printErrorMessage(ErrorTypes errorType){
+    if(errorType == ErrorTypes::IncorrectCommitType){
+        cout << "Incorrect commit type!" << endl;
+    }
+    else if(errorType == ErrorTypes::NoCommitType){
+        cout << "Please enter the type of commits you which to use to generate release notes" << endl;
+    }
+    cout << "Please choose either n (Normal Commits) or m (Merged Commits)" << endl;
+    cout << "Expected Syntax:" << endl;
+    cout << "1 - release_notes_generator n" << endl;
+    cout << "2 - release_notes_generator m [s/f] (short or full release notes - by default short)";
+}
+
+int main(int argc, char* argv[]){
+    if(argc > 1){
+        if(strcmp(argv[1], "n") == 0){
+        generateReleaseNotes(Commits::Normal, OutputTypes::Markdown);
+        }
+        else if(strcmp(argv[1], "m") == 0){
+            generateReleaseNotes(Commits::Merge, OutputTypes::Markdown);
+        }
+        else{
+            // Can enhance this part by using options -s, -f, etc. 
+            // And also check git and other popular tools on how to send better help messages
+            printErrorMessage(ErrorTypes::IncorrectCommitType);
+        }
+    }
+    else{
+        printErrorMessage(ErrorTypes::NoCommitType);
+    }
+
+    return 0;
+}
